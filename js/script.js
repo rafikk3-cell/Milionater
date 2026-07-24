38564045443c21927f14c4de8f438e8b49c29d4b
rafikk3-cell <margush.virabyan@tumo.org>
2026-07-24 20:12:03 +0400

    css and js

---
+++ js/script.js
@@ -0,0 +1,20 @@
+'use strict';
+
+let mainGame = document.querySelector('.game-blok'),
+let mainGame = document.querySelector('.game-wrap'),
+startBtn = document.querySelector('.start-Btn'),
+endBtn = document.querySelector('.end-Btn'),
+btnAnswers = document. querySelectorAll('.answer'),
+btnAnswers = document. querySelectorAll('.questions'),
+helpBtns  = document. querySelectorAll('.hints-help'),
+winsBlock = document. querySelectorAll('.wins-block'),
+helpFifty  = document. querySelector('.fifty-fifty'),
+helpHall  = document. querySelector('.hall-help'),
+helpFrind  = document. querySelector('.call-frind'),
+helpAI  = document. querySelector('.ai-help'),
+game  = document. querySelector('.game'),
+////////////////========================>
+ 
+ let changeQuestion = document.getElementById('hintBox')
+ let extraQuestion = document.getElementById('extra')
+ let flagExtra = true
